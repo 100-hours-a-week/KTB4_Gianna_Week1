2b8eb3f01ff31135562f42d1b77becfe2ba38794
Stella <yeobinlee1210@gmail.com>
2026-05-23 01:29:02 +0900

    refactor(Constants): 상수처리

---
+++ src/main/java/org/example/Constants.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Constants {
+    public static final int LOTTERY_COUNT = 6;
+    public static final int SPITO_PRICE = 1000;
+    public static final int SPITO_MAX_NUMBER = 9;
+    public static final int SPITO_WINNINGNUM_COUNT = 1;
+}
