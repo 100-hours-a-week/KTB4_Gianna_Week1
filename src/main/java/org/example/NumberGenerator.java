00c4982614a596a1b0dfc963674c910bf64077b4
Stella <yeobinlee1210@gmail.com>
2026-05-23 01:30:53 +0900

    feat(NumberGenerator): 랜덤 숫자 추출 로직 컴포지션으로 변경

---
+++ src/main/java/org/example/NumberGenerator.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.*;
+
+public class NumberGenerator {
+    private ArrayList<Integer> row;
+    public NumberGenerator(){}
+    private int pickRandomNum(int maxNumber){
+        Random random = new Random();
+        return random.nextInt(maxNumber) + 1;
+    }
+
+    public List<Integer> draw(int maxNumber, int count){
+        //6자리 랜덤 뽑기
+        HashSet<Integer> hashSet = new HashSet<>();
+        while (hashSet.size() < count) {
+            int number = pickRandomNum(maxNumber);
+            hashSet.add(number);
+        }
+        List<Integer> sortedList = new ArrayList<>(hashSet);
+        sortedList.sort(Comparator.naturalOrder());
+        return sortedList;
+    }
+}
